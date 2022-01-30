1db6b7ead371238be65e99827f8c28e5dc373c5a
root <zicodehub@gmail.com>
2022-01-30 08:17:00 +0000

    Second deployable

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import "./App.css"
 
 function JitsiMeetComponent() {
-    const [loading, setLoading] = useState(true);
+    const [room, setRoom] = useState('')
+    const [name, setName] = useState('')
+    const [start, setStart] = useState(false)    
+    const [running, setRunning] = useState(false)    
+    
+    let id = 1
+    
     const containerStyle = {
-        width: '800px',
-        height: '400px',
+        width: '100%',
+        height: '100vh',
+        display: "flex"
     };
 
     const jitsiContainerStyle = {
-        display: (loading ? 'none' : 'block'),
-        width: '100%',
+        display:  'block',
+        width: '50%',
+        height: '100%',
+    }
+
+    const paintContainerStyle = {
+        display:  'block',
+        width: '50%',
         height: '100%',
     }
 
@@ -17,9 +31,12 @@ function JitsiMeetComponent() {
         try {
             const domain = 'meet.jit.si';
             const options = {
-                roomName: 'roomName',
-                height: 400,
-                parentNode: document.getElementById('jitsi-container'),
+                roomName: room,
+                userInfo: {
+                    email: 'email@jitsiexamplemail.com',
+                    displayName: name
+                },
+                parentNode: document.getElementById('jitsi-container'+id),
                 interfaceConfigOverwrite: {
                     filmStripOnly: false,
                     SHOW_JITSI_WATERMARK: false,
@@ -30,28 +47,52 @@ function JitsiMeetComponent() {
             };
 
             const api = new window.JitsiMeetExternalAPI(domain, options);
-            api.addEventListener('videoConferenceJoined', () => {
-                console.log('Local User Joined');
-                setLoading(false);
-                api.executeCommand('displayName', 'MyName');
-            });
+            window.zo = api
 
         } catch (error) {
             console.error('Failed to load Jitsi API', error);
         }
     }
     
+    useEffect(()=> {
+        id++
+        if(start && !running) setRunning(true)
+        try {
+            setTimeout(() => {
+                document.getElementById('jitsiConferenceFrame1').style.display = 'none'} , 5000)
+        } catch {}
+    })
 
-    useEffect(() => {
-        // verify the JitsiMeetExternalAPI constructor is added to the global..
-        if (window.JitsiMeetExternalAPI) startConference();
-        else alert('Jitsi Meet API script not loaded');
-    }, []);
-
+    const renderJitsi = () => (
+        <div id={"jitsi-container"+id} style={jitsiContainerStyle} />
+    )
+    
+    const renderPaint = () => {
+        if(running) return <iframe src={"https://chatpaint.herokuapp.com/?&name="+name} style={paintContainerStyle} />
+        else if (start) return <h1> Espace de dessin collectif </h1>
+        else return null
+    }
+    const renderForm = () => (
+        <form>
+            <input id='room' type='text' placeholder='Room' 
+                value={room} onChange={(e) => setRoom(e.target.value)} />
+            <input id='name' type='text' placeholder='Name' 
+                value={name} onChange={(e) => setName(e.target.value)} />
+            <button onClick={handleClick} type='submit'>
+                Commencer
+            </button>
+        </form>
+    )
+    
+    const handleClick = event => {
+        event.preventDefault()
+        if (room !== '' && name !== '' && !start) setStart(true)
+    }
     return (
         <div style={containerStyle} >
-            {loading && <h1>Chargement......</h1>}
-            <div id="jitsi-container" style={jitsiContainerStyle} />
+            {renderJitsi()}
+            {renderPaint()}
+            {start ? ( running ?  null : startConference() ) : renderForm()}
         </div>
     );
 }

b1a447f344f41f5849638fbef99066200e4bcf2e
root <zicodehub@gmail.com>
2022-01-30 03:01:52 +0000

    second

--- src/App.js
+++ src/App.js
@@ -43,16 +43,10 @@ const App = () => {
 
     const handleJitsiIFrameRef1 = iframeRef => {
         iframeRef.style.border = '10px solid cadetblue';
-        iframeRef.style.background = 'cadetblue';
-        iframeRef.style.height = '400px';
+        iframeRef.style.background = 'yellow';
+        iframeRef.style.height = '90%';
     };
 
-    const handleJitsiIFrameRef2 = iframeRef => {
-        iframeRef.style.marginTop = '10px';
-        iframeRef.style.border = '10px dashed tomato';
-        iframeRef.style.padding = '5px';
-        iframeRef.style.height = '400px';
-    };
 
     const handleApiReady = (apiObj, ref) => {
         ref.current = apiObj;
@@ -66,72 +60,6 @@ const App = () => {
             knockingParticipant: handleKnockingParticipant
         });
     };
-
-    // Multiple instances demo
-    const renderNewComp = () => {
-        if (!showNew) {
-            return null;
-        }
-
-        return (
-            <JitsiMeeting
-                domain='meet.jit.si'
-                roomName='JitsiMeetingComponentDemo'
-                onApiReady={externalApi => handleApiReady(externalApi, apiRefNew)}
-            />
-        );
-    };
-
-    const renderButtons = () => (
-        <div style={{ margin: '15px 0' }}>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center'
-            }}>
-                <button
-                    type='text'
-                    title="Click to execute toggle raise hand command"
-                    style={{
-                        border: 0,
-                        borderRadius: '6px',
-                        fontSize: '14px',
-                        background: '#ff9b42',
-                        color: 'white',
-                        padding: '12px 46px',
-                        margin: '2px 2px'
-                    }}
-                    onClick={() => apiRef.current.executeCommand('toggleRaiseHand')}
-                >Raise hand</button>
-                <button
-                    type='text'
-                    title="Click to approve/reject knocking participant"
-                    style={{
-                        border: 0,
-                        borderRadius: '6px',
-                        fontSize: '14px',
-                        background: '#0376da',
-                        color: 'white',
-                        padding: '12px 46px',
-                        margin: '2px 2px'
-                    }}
-                    onClick={() => resolveKnockingParticipants(apiRef, ({ name }) => !name.includes('test'))}
-                >Resolve lobby</button>
-                <button
-                    type='text'
-                    style={{
-                        border: 0,
-                        borderRadius: '6px',
-                        fontSize: '14px',
-                        background: '#a7a7a7',
-                        color: 'white',
-                        padding: '12px 46px',
-                        margin: '2px 2px'
-                    }}
-                    onClick={() => toggleShowNew(!showNew)}
-                >Toggle new instance</button>
-            </div>
-        </div>
-    );
 
     const renderLog = () => logItems.map(
         (item, index) => (
@@ -156,22 +84,20 @@ const App = () => {
                 fontFamily: 'sans-serif',
                 textAlign: 'center'
             }}>Projet de Programmation répartie</h1>
+        
             <JitsiMeeting
                 domain="meet.jit.si"
                 spinner={renderSpinner}
                 onApiReady={externalApi => handleApiReady(externalApi, apiRef)}
                 getIFrameRef={handleJitsiIFrameRef1}
                 configOverwrite={{
-                    startWithAudioMuted: true,
+                    startWithAudioMuted: false,
                     disableModeratorIndicator: true,
                     startScreenSharing: false,
                     enableEmailInStats: false
                 }}
             />
 
-            
-            {renderNewComp()}
-            {renderLog()}
         </>
     );
 };
